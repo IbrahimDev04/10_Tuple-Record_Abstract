d6bcea4a5d7902e80d6e3299b9adcbdfb79ee154
muradramazanov02 <tu71rptff@code.edu.az>
2024-04-04 13:01:12 +0400

    add content

---
+++ 10_Tuple/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _10_Tuple_Record_Abstract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            (int, string) tup1 = (18, "ibrahim");
+            (int, string, int) tup2 = (18, "ibrahim",75);
+            (int, string, decimal, float) tup3 = (18, "ibrahim", 43m , 34f);
+        }
+    }
+}
